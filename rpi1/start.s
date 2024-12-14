5561ce3d9f483329a569b657dcf8f1caf8b71d8c
Gautier Berthou <gautier.berthou@outlook.fr>
2024-12-14 14:02:24 +0100

    Proper bss/data loader

--- rpi1/start.s
+++ rpi1/start.s
@@ -1,6 +1,29 @@
 .section .text
 .global ResetHandler
 ResetHandler:
+    ;@ Zero bss section
+    ldr r0, =__bss_start
+    ldr r1, =__bss_end
+    mov r2, #0
+bssloop:
+    cmp r0, r1
+    beq bssloopout
+    str r2, [r0], #4
+    b bssloop
+bssloopout:
+
+    ;@ Copy rodata into data
+    ldr r0, =__data_start
+    ldr r1, =__data_end
+    ldr r2, =__data_load_start
+dataloop:
+    cmp r0, r1
+    beq launch
+    ldr r3, [r2], #4
+    str r3, [r0], #4
+    b dataloop
+
+launch:
     ;@ Set interrupt vector base address to 0xFFFF0000
     ;@mrc p15, 0, r0, c1, c0, 0
     ;@orr r0, #0x2000 ;@ r0 |= (1 << 13) V bit
